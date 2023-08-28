6803e555b8e653336d9aef07ef0eeca29582495b
Joseph C <joseph123749@gmail.com>
2023-08-27 19:10:13 -0500

    Implement Goku Bot

---
+++ commands/Funni/goku.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const messages = 
+["Di you think it's okay to discriminate based on horoscope signs?", 
+"Do you think incest didn't cause deformed babies, etc, do you think it should be legal", 
+"Guys is it against company policy to have romantic relationship with coworkers", 
+"I want Google \nI want Google\nI want Google\nI want Google\nv\nI want Google",
+"I peed white\nWhat's wrong with me\n😥\nIt was like milk @polar",
+"Do you think virginity is a social construct",
+"I wish they changed the rule for presidents that only us borns can become one :worrykms:",
+"Why is a test engineer recommending a cp hentai",
+"If thecco is the republican, I must be the one offsetting his bigotry",
+"Anyone here got a NPC kink?",
+"@polar you ever thought about when you're eating popcorn that you're just eating bunch of busted nuts?",
+"If thecco has esex, we will too\n🔥🔥🔥🔥🔥🔥🔥"
+]
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('goku')
+		.setDescription('goku got a question'),
+
+	async execute(interaction) {
+        await interaction.reply(messages[Math.floor(Math.random()*messages.length)]);
+	},
+};
